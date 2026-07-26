4e2b5a092ac01ee1d31bd9899799b35dfa7b7f5f
17thToasterOnTheLeft <drshashiibhushann@gmail.com>
2026-07-26 10:34:11 +0530

    Create statistics.py
    i dont know what i am doing

---
+++ statistics.py
@@ -0,0 +1,165 @@
+# Basic Statistics
+mean()
+median()
+mode()
+multimode()
+minimum()
+maximum()
+range()
+sum_values()
+product()
+
+# Measures of Spread
+variance()
+sample_variance()
+standard_deviation()
+sample_standard_deviation()
+mean_absolute_deviation()
+median_absolute_deviation()
+coefficient_of_variation()
+interquartile_range()
+
+# Percentiles
+percentile()
+quartiles()
+deciles()
+percentile_rank()
+
+# Means
+geometric_mean()
+harmonic_mean()
+weighted_mean()
+trimmed_mean()
+winsorized_mean()
+root_mean_square()
+
+# Standard Scores
+z_score()
+z_scores()
+
+# Shape
+skewness()
+kurtosis()
+
+# Correlation
+covariance()
+pearson()
+spearman()
+kendall()
+correlation_matrix()
+
+# Regression
+linear_regression()
+multiple_regression()
+polynomial_regression()
+predict()
+r_squared()
+adjusted_r_squared()
+residuals()
+
+# Hypothesis Testing
+t_test()
+paired_t_test()
+independent_t_test()
+anova()
+chi_square()
+f_test()
+levene_test()
+mann_whitney()
+wilcoxon()
+kruskal()
+friedman()
+
+# Confidence
+confidence_interval()
+margin_of_error()
+sample_size()
+power_analysis()
+
+# Probability
+factorial()
+permutations()
+combinations()
+bayes()
+conditional_probability()
+union_probability()
+intersection_probability()
+expected_value()
+
+# Distributions
+normal_pdf()
+normal_cdf()
+binomial()
+poisson()
+geometric_distribution()
+negative_binomial()
+uniform_distribution()
+beta_distribution()
+gamma_distribution()
+weibull_distribution()
+chi_square_distribution()
+student_t_distribution()
+f_distribution()
+exponential_distribution()
+
+# Data Processing
+normalize()
+standardize()
+minmax_scale()
+robust_scale()
+remove_outliers()
+detect_outliers()
+frequency_table()
+group_data()
+rank_data()
+
+# Time Series
+moving_average()
+exponential_moving_average()
+forecast()
+
+# Visualization Helpers
+histogram()
+bar_chart()
+line_chart()
+scatter_plot()
+pie_chart()
+box_plot()
+violin_plot()
+density_plot()
+heatmap()
+qq_plot()
+pair_plot()
+parallel_coordinates()
+
+# Dataset Utilities
+read_csv()
+read_excel()
+read_json()
+read_sql()
+export_csv()
+export_excel()
+
+# Random Data
+random_sample()
+bootstrap()
+shuffle()
+random_dataset()
+
+# Machine Learning Metrics
+accuracy()
+precision()
+recall()
+specificity()
+f1_score()
+confusion_matrix()
+roc_curve()
+auc()
+
+# Miscellaneous
+entropy()
+gini_index()
+distance()
+euclidean_distance()
+manhattan_distance()
+cosine_similarity()
